82edc82b2377a52ce2af37967cd8f5d7289a439e
Jake McKown <jbmckown@gmail.com>
2020-01-24 13:36:12 -0700

    Add unit tests

---
+++ test/unit/Preprocessor.test.js
@@ -0,0 +1,61 @@
+/* eslint-env jest */
+
+const { Preprocessor } = require('../../lib/Preprocessor.js')
+
+describe('Preprocessor', () => {
+  it('Should be defined', () => {
+    expect(Preprocessor).toBeDefined()
+  })
+
+  describe('Preprocessor instance', () => {
+    it('Should be defined', () => {
+      const pp = new Preprocessor()
+      
+      expect(pp).toBeDefined()
+    })
+
+    it('Should have method "addRule"', () => {
+      const pp = new Preprocessor()
+      
+      expect(pp).toHaveProperty('addRule')
+      expect(pp.addRule).toBeInstanceOf(Function)
+    })
+
+    it('Should have method "preprocess"', () => {
+      const pp = new Preprocessor()
+      
+      expect(pp).toHaveProperty('preprocess')
+      expect(pp.preprocess).toBeInstanceOf(Function)
+    })
+
+    it('Should allow a rule to replace tokens', () => {
+      const pp = new Preprocessor()
+      const pattern = /J/
+      const src = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      const dest = 'ABCDEFGHI j KLMNOPQRSTUVWXYZ'
+
+      pp.addRule(pattern, () => {
+        return ' j '
+      })
+      
+      expect(pp.preprocess(src)).toBe(dest)
+    })
+
+    it('Should allow a rule to define new rules', () => {
+      const pp = new Preprocessor()
+      const pattern = /#define\s+([a-z_@#$&;.?][\w@#$&;.?]*)\s+([^;])[;\n]\s*/i
+      const src = '#define TEST 1; var x = TEST; return x;'
+      const dest = 'var x = 1; return x;'
+
+      pp.addRule(pattern, (statement, name, value) => {
+        pp.addRule(new RegExp(name), () => {
+          return value
+        })
+      
+        return ''
+      })
+      
+      expect(pp.preprocess(src)).toBe(dest)
+    })
+  })
+})
